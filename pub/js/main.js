84564552e591bfab59834e661be30f708d8e22b9
Joakim Rosqvist <joakimrosqvist1@gmail.com>
2019-10-19 09:56:11 +0200

    first commit

---
+++ pub/js/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#mobile-menu").on("click",function(){$(this).toggleClass("open")})}),$(document).ready(function(){$("#mobile-menu").on("click",function(){$("#main-nav ul").toggleClass("open")})});
